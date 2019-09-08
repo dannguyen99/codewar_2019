186b5ece70c01a875f0c65db3fb68a62b574d07f
dannguyen <dannguyen0801@gmail.com>
2019-09-08 15:35:36 +0700

    finished problem 1

---
+++ problem1.py
@@ -0,0 +1,10 @@
+def oddNumbers(s):
+    choices = list(range(0, 10))
+    possible = 0
+    used = []
+    print(choices)
+    for i in choices:
+        def sum = 0
+        for j in s:
+
+oddNumbers("ab")
